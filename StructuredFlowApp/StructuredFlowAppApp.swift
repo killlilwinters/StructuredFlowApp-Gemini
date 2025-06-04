ffc4fa400582e79178fa7e8e58741f6368dd76de
Maksym Horobets <makswinters@outlook.com>
2025-06-04 17:13:15 +0300

    Initial Commit

---
+++ StructuredFlowApp/StructuredFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructuredFlowAppApp.swift
+//  StructuredFlowApp
+//
+//  Created by Maksym Horobets on 04.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct StructuredFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
